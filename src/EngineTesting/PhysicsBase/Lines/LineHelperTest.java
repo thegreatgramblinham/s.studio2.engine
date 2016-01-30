8242192a71fb4654e14373d7b326b2953e7d70fa
Sam Hudock <samhudock@gmail.com>
2016-01-30 13:05:24 -0500

    Line helper tests.

---
+++ src/EngineTesting/PhysicsBase/Lines/LineHelperTest.java
@@ -0,0 +1,70 @@
+package EngineTesting.PhysicsBase.Lines;
+
+import PhysicsBase.Lines.LineHelper;
+import PhysicsBase.Lines.NLine;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class LineHelperTest
+{
+
+    //Private Variables
+    private NLine _line1;
+    private NLine _line2;
+
+    //Tests
+    @Test
+    public void testLineSegmentIntersect1() throws Exception
+    {
+        //perfect vertical and horizontal orientation
+        _line1 = new NLine(new Point(1,0), new Point(1,2));
+        _line2 = new NLine(new Point(0,1), new Point(2,1));
+
+        Assert.assertTrue(LineHelper.Intersect(_line1, _line2));
+    }
+
+    @Test
+    public void testLineSegmentIntersect2() throws Exception
+    {
+        //turned 45 degrees from above
+        _line1 = new NLine(new Point(0,0), new Point(2,2));
+        _line2 = new NLine(new Point(0,2), new Point(2,0));
+
+        Assert.assertTrue(LineHelper.Intersect(_line1, _line2));
+    }
+
+    @Test
+    public void testLineSegmentIntersect3() throws Exception
+    {
+        //vertical parallel
+        _line1 = new NLine(new Point(0,0), new Point(0,2));
+        _line2 = new NLine(new Point(2,0), new Point(2,2));
+
+        Assert.assertFalse(LineHelper.Intersect(_line1, _line2));
+    }
+
+    @Test
+    public void testLineSegmentIntersect4() throws Exception
+    {
+        //horizontal parallel
+        _line1 = new NLine(new Point(0,0), new Point(2,0));
+        _line2 = new NLine(new Point(0,1), new Point(2,1));
+
+        Assert.assertFalse(LineHelper.Intersect(_line1, _line2));
+    }
+
+    @Test
+    public void testLineSegmentIntersect5() throws Exception
+    {
+        //same startpoint, same angle, different distance.
+        _line1 = new NLine(new Point(0,0), new Point(0,1));
+        _line2 = new NLine(new Point(0,0), new Point(0,2));
+
+        Assert.assertFalse(LineHelper.Intersect(_line1, _line2));
+    }
+}
--- src/PhysicsBase/Lines/LineHelper.java
+++ src/PhysicsBase/Lines/LineHelper.java
@@ -10,7 +10,7 @@ public final class LineHelper
     private LineHelper(){}
 
     //Public Methods
-    public boolean Intersect(NLine l1, NLine l2)
+    public static boolean Intersect(NLine l1, NLine l2)
     {
         //todo need check for colinear
         return (CheckSegmentTriangle(l1.point1, l2.point1, l2.point2) !=
@@ -20,7 +20,7 @@ public final class LineHelper
     }
 
     //Private Methods
-    public boolean CheckSegmentTriangle(Point p1, Point p2, Point q1)
+    private static boolean CheckSegmentTriangle(Point p1, Point p2, Point q1)
     {
         return ((q1.getY() - p1.getY()) * (p2.getX() - p1.getX()))
                 > ((p2.getY() - p1.getY()) * (q1.getX()-p1.getX()));
