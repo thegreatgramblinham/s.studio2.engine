6a575d01b4335cab1d3ba7fe6ed4fe7e5c6f88ff
Sam <samhudock@gmail.com>
2016-02-20 17:01:25 -0800

    Adding AnimationHelper class.

---
+++ src/Animation/BufferedImageHelper.java
@@ -0,0 +1,37 @@
+package Animation;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
+public class BufferedImageHelper
+{
+    private static Image spriteSheet;
+    private static ImageView spriteSheetView;
+    private static final int TILE_SIZE = 32;
+
+    public static Image loadSprite(String file) {
+
+        try {
+            spriteSheet = new Image(new File(file).toURI().toString());
+            spriteSheetView = new ImageView(spriteSheet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return spriteSheet;
+    }
+
+    public static ImageView getSprite(int xGrid, int yGrid) {
+
+        if (spriteSheet == null) {
+            spriteSheet = loadSprite("AnimationSpriteSheet");
+        }
+
+        spriteSheetView.setViewport(new Rectangle2D(xGrid * TILE_SIZE, yGrid * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+        spriteSheetView.
+        return spriteSheetView;
+    }
+}
