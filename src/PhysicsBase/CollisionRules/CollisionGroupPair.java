b781b670f587386db38a0082a793f47301e12ee8
Sam Hudock <samhudock@gmail.com>
2016-03-22 16:20:15 -0400

    Continuing rule manager implementation.

--- src/PhysicsBase/CollisionRules/CollisionGroupPair.java
+++ src/PhysicsBase/CollisionRules/CollisionGroupPair.java
@@ -3,20 +3,18 @@ package PhysicsBase.CollisionRules;
 import PhysicsBase.CollisionCollections.CollisionGroup;
 import PhysicsBase.CollisionRules.enums.CollisionRule;
 
-public class CollisionRulePair
+public class CollisionGroupPair
 {
     //Variables
     public CollisionGroup group1;
     public CollisionGroup group2;
-    public CollisionRule rule;
 
     //Constructor
-    public CollisionRulePair(CollisionGroup grp1, CollisionGroup grp2,
-                             CollisionRule rule)
+    public CollisionGroupPair(CollisionGroup grp1, CollisionGroup grp2,
+                              CollisionRule rule)
     {
         group1 = grp1;
         group2 = grp2;
-        this.rule = rule;
     }
 
     //Get Methods
@@ -25,13 +23,12 @@ public class CollisionRulePair
 
     //Public Methods
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o)
+    {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        CollisionRulePair that = (CollisionRulePair) o;
-
-        if(this.rule != that.rule) return false;
+        CollisionGroupPair that = (CollisionGroupPair) o;
 
         //As long as the object contains the same two groups they're equal.
         //They can be assigned to either variable.
@@ -42,13 +39,12 @@ public class CollisionRulePair
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         int result = group1 != null ? group1.hashCode() : 0;
         result = 31 * result + (group2 != null ? group2.hashCode() : 0);
-        result = 31 * result + (rule != null ? rule.hashCode() : 0);
         return result;
     }
-
 
     //Private Methods
 
--- src/PhysicsBase/CollisionRules/CollisionRuleManager.java
+++ src/PhysicsBase/CollisionRules/CollisionRuleManager.java
@@ -1,14 +1,16 @@
 package PhysicsBase.CollisionRules;
 
+import GameObjectBase.GameWorldObject;
 import PhysicsBase.CollisionCollections.CollisionGroup;
+import PhysicsBase.CollisionRules.enums.CollisionRule;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class CollisionRuleManager
 {
     //Private Fields
-    private HashMap<CollisionGroup, HashSet<CollisionRulePair>> _groupToRules;
+    private HashMap<CollisionGroupPair, CollisionRule> _groupToRules;
+    private HashMap<GameWorldObject, CollisionGroup> _objectToGroup;
 
     //Variables
 
@@ -19,7 +21,16 @@ public class CollisionRuleManager
     //Set Methods
 
     //Public Methods
+    public void AddRule(CollisionGroupPair pair, CollisionRule rule)
+    {
+        _groupToRules.put(pair, rule);
+    }
 
     //Private Methods
+    private void Init()
+    {
+        _groupToRules = new HashMap<>();
+        _objectToGroup = new HashMap<>();
+    }
 
 }
