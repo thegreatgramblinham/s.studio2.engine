37fbad3bc155cd3b3a3f8597752f3ebdfc0dd33e
Sam <samhudock@gmail.com>
2016-05-01 11:47:40 -0400

    Adding visiblity and disabled properties for the menus.

--- src/Menus/MenuItem.java
+++ src/Menus/MenuItem.java
@@ -6,9 +6,42 @@ import java.awt.*;
 
 public abstract class MenuItem extends BoundedObject
 {
+    //Private Variables
+    private boolean _isVisible;
+    private boolean _isDisabled;
+
     //Constructor
     public MenuItem(Rectangle rect)
     {
         super(rect);
+        Init();
+    }
+
+    //Get Methods
+    public boolean GetIsVisible()
+    {
+        return _isVisible;
+    }
+    public boolean SetIsDisabled()
+    {
+        return _isDisabled;
+    }
+
+    //Set Methods
+    public void SetIsDisabled(boolean isDisabled)
+    {
+        this._isDisabled = isDisabled;
+    }
+
+    public void SetIsVisible(boolean isVisible)
+    {
+        this._isVisible = isVisible;
+    }
+
+    //Private Methods
+    private void Init()
+    {
+        _isVisible = true;
+        _isDisabled = false;
     }
 }
